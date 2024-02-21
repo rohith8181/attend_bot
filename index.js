64cbf8d435e4b9403d0e0a881aa387f32d648b0b
ROHITH <rohithperala81@gmail.com>
2024-02-21 18:08:29 +0530

    error handling

--- index.js
+++ index.js
@@ -6,58 +6,61 @@ const cron = require('node-cron');
 const PORT = process.env.PORT || 5000
 
 const autoattend = async () => {
-    console.log("Scheduled function started...")
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    try {
+        console.log("Scheduled function started...")
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-    await page.goto('https://portal.uptricksservices.com/dashboard-page/');
+        await page.goto('https://portal.uptricksservices.com/dashboard-page/');
 
-    const userselector = 'input[type="text"][name="log"]';
-    await page.waitForSelector(userselector);
-    await page.type(userselector, process.env.USER_NAME)
-
-
-    const passselector = 'input[type="password"][name="pwd"]';
-    await page.waitForSelector(passselector);
-    await page.type(passselector, process.env.PASSWORD)
-
-    const subselctor = 'button[type="submit"]';
-    await page.waitForSelector(subselctor);
-    await page.click(subselctor);
+        const userselector = 'input[type="text"][name="log"]';
+        await page.waitForSelector(userselector);
+        await page.type(userselector, process.env.USER_NAME)
 
 
-    const punchselector = '#menu-item-235';
-    await page.waitForSelector(punchselector);
-    await page.click(punchselector);
+        const passselector = 'input[type="password"][name="pwd"]';
+        await page.waitForSelector(passselector);
+        await page.type(passselector, process.env.PASSWORD)
 
-    await new Promise(resolve => setTimeout(resolve, 5000));
+        const subselctor = 'button[type="submit"]';
+        await page.waitForSelector(subselctor);
+        await page.click(subselctor);
 
-    const attendselector = '#aio_clock_button';
-    await page.click(attendselector);
 
-    const hourinterval = 7 * 60 * 60 * 1000;
-    await new Promise(resolve => setTimeout(resolve, hourinterval));
+        const punchselector = '#menu-item-235';
+        await page.waitForSelector(punchselector);
+        await page.click(punchselector);
 
-    const endmin = Math.floor(Math.random() * 28) + 30;
-    const mininterval = endmin * 60 * 1000;
-    await new Promise(resolve => setTimeout(resolve, mininterval));
+        await new Promise(resolve => setTimeout(resolve, 5000));
 
-    await page.click(attendselector);
+        const attendselector = '#aio_clock_button';
+        await page.click(attendselector);
 
-    console.log("Scheduled Function Ended...")
-    await browser.close();
+        const hourinterval = 7 * 60 * 60 * 1000;
+        await new Promise(resolve => setTimeout(resolve, hourinterval));
+
+        const endmin = Math.floor(Math.random() * 28) + 30;
+        const mininterval = endmin * 60 * 1000;
+        await new Promise(resolve => setTimeout(resolve, mininterval));
+
+        await page.click(attendselector);
+
+        console.log("Scheduled Function Ended...")
+        await browser.close();
+    } catch (err) {
+        console.log("Error in autoattend function", err)
+    }
 }
 
-const scheduleCronJob = () => {
-    const min = Math.floor(Math.random() * 30);
-    cron.schedule(`${min} 9 * * 1-5`, autoattend, { timezone: 'Asia/Kolkata' });
+const scheduleCronJob = async () => {
+    let min = Math.floor(Math.random() * 30);
+    min = min * 60 * 1000;
+    await new Promise(resolve => setTimeout(resolve, min));
+    autoattend();
 };
 
-scheduleCronJob();
+cron.schedule(`50 8 * * 1-5`, scheduleCronJob, { timezone: 'Asia/Kolkata' });
 
-cron.schedule('0 0 * * *', () => {
-    scheduleCronJob();
-}, { timezone: 'Asia/Kolkata' });
 
 app.get('/', (req, res) => {
     res.send("Server is Working...")
